fb6f4b0befa75e7df6a0600012562dfed5bfeec2
duyhoang3820 <duyhoang3820@gmail.com>
2021-07-03 15:58:58 +0700

    update cinemasystem

--- src/pages/TrangChu/CinemaSystem/TabCumRap.js
+++ src/pages/TrangChu/CinemaSystem/TabCumRap.js
@@ -10,6 +10,47 @@ export default function TabCumRap(props) {
     const { TabPane } = Tabs;
 
     let cumRap = props.cumRap;
+    console.log('cumRap', cumRap);
+    const splitCumRap = (tenCumRap) => {
+        return tenCumRap.split(' - ')
+    }
+    const styleClass = (tenCumRap) => {
+        let systems = [
+            {
+                name: 'BHD Star',
+                color: '#8bc541'
+            },
+            {
+                name: 'CGV',
+                color: 'red'
+            },
+            {
+                name: 'CNS',
+                color: '#df0d7a'
+            },
+            {
+                name: 'Lotte',
+                color: '#ca4137'
+            },
+            {
+                name: 'MegaGS',
+                color: '#e5a813'
+            },
+            {
+                name: 'GLX',
+                color: 'orange'
+            },
+        ]
+        let arr = systems.find(item => {
+            // console.log('item',item);
+            // console.log('tenCumRap',tenCumRap);
+            // console.log('text',tenCumRap.includes(item.name));
+
+            return tenCumRap.includes(item.name)
+        })
+        // console.log(arr);
+        return arr
+    }
     return (
         <div>
             <Tabs tabPosition="top" defaultActiveKey="0">
@@ -20,7 +61,10 @@ export default function TabCumRap(props) {
                                 <img style={{ borderRadius: '3px', }} src={imgcumRap} width='50px' height="50px"></img>
                             </div>
                             <div className="col-8">
-                                <p className="tenCumRap m-0">{cr.tenCumRap}</p>
+                                <p className="tenCumRap m-0">
+                                    <span style={{ color: styleClass(cr.tenCumRap).color }}>
+                                    {splitCumRap(cr.tenCumRap)[0]}</span>
+                                    <span style={{ color: 'black' }}> - {splitCumRap(cr.tenCumRap)[1]}</span></p>
                                 <p className="diaChi m-0">{cr.diaChi}</p>
                                 <p className="chiTiet m-0">[Chi tiết]</p>
                             </div>
